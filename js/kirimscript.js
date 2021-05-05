b17bd9742d0f77e1f8dfa2939991377a4c516911
Fikrul Akhyar <fikrul18@mhs.unsyiah.ac.id>
2021-05-05 23:46:19 +0700

    Merge remote-tracking branch 'origin/1908107010039' into main

---
+++ js/kirimscript.js
@@ -0,0 +1,24 @@
+const flashData = $('#flash').data('flashdata');
+console.log(flashData);
+
+if (flashData) {
+    if (flashData == "Berhasil") {
+        Swal.fire({
+            title: "Pengajuan " + flashData,
+            text: "Usulan Akan Diseleksi Untuk Ditambahkan Ke Program Kerja",
+            icon: "success",
+            button: "OK"
+        });
+    } else {
+        Swal.fire({
+            title: "Pengajuan " + flashData,
+            text: "Harap Mengisi Semua Form",
+            icon: "warning",
+            button: "OK"
+        });
+    }
+}
+
+$('#vote').click(function () {
+    
+});
