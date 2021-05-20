597cb37201a2b27abf4fa29fe00b39aace81f9ba
ulfiaakhairani <ulfia199@mhs.unsyiah.ac.id>
2021-05-20 11:08:00 +0700

    Pembaharuan pada proses

--- login.php
+++ login.php
@@ -1,4 +1,6 @@
-<?php require '../config/config.php'; ?>
+<?php
+session_start();
+require 'config.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -13,11 +15,11 @@
     <title>Login</title>
 
     <!-- Custom fonts for this template-->
-    <link href="<?= base_url ?>/assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="<?= base_url ?>/assets/css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
 
 </head>
 
@@ -34,7 +36,7 @@
                         <div class="col-lg-6 pb-4">
                             <div class="p-4">
                                 <div class="text-left mb-5">
-                                    <a href="<?= base_url ?>"><img src="<?= base_url .'/assets/img/logo.png'; ?>"></a>
+                                    <a href="index"><img src="img/logo.png"></a>
                                 </div>
                                 <div style="color: #137F7F; padding-left: 75px; padding-right: 75px;">
                                     <h3 style="font-weight:750;">Sistem Pengusulan Program Desa Batoh</h3>
@@ -45,16 +47,18 @@
 
 
                                 <div style="padding-left: 75px; padding-right: 75px;">
-                                    <?php if (!empty($_GET['message'])) { echo $_GET['message']; } ?>
+                                    <?php if (!empty($_SESSION['message'])) {
+                                        echo $_SESSION['message'];
+                                    } ?>
 
-                                    <form class="user" method="post" action="<?= base_url .'/login/proses.php'; ?>">
+                                    <form class="user" method="post" action="loginproses">
                                         <div class="form-group">
-                                            <input type="tel" class="form-control form-control-user" id="nik" name="nik" placeholder="NIK" value="">
-                                           <!--  <?//= form_error('nik', '<small class="text-danger pl-3">', '</small>'); ?> -->
+                                            <input type="tel" class="form-control form-control-user <?= (isset($_SESSION['nikErr'])) ? 'is-invalid' : 'is_valid' ?>" id="nik" name="nik" placeholder="NIK">
+                                            <div class="invalid-feedback"><?= $_SESSION['nikErr']; ?></div>
                                         </div>
                                         <div class="form-group">
-                                            <input type="password" class="form-control form-control-user" id="password" name="password" placeholder="Password">
-                                           <!--  <?//= form_error('password', '<small class="text-danger pl-3">', '</small>'); ?> -->
+                                            <input type="password" class="form-control form-control-user <?= (isset($_SESSION['passErr'])) ? 'is-invalid' : 'is_valid' ?>" id="password" name="password" placeholder="Password">
+                                            <div class="invalid-feedback"><?= $_SESSION['passErr'];; ?></div>
                                         </div>
                                         <div class="form-group" style="display: flex; justify-content:space-between;">
                                             <div class="custom-control custom-checkbox small">
@@ -76,17 +80,18 @@
                                 <hr>
 
                                 <div class="text-center ">
-                                    <a class="small font-weight-bold" style="color: #137F7F" href="<?= base_url .'/registration'; ?>">Buat Akun</a>
+                                    <a class="small font-weight-bold" style="color: #137F7F" href="signup">Buat Akun</a>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-lg-6" style="background-image: url('<?= base_url .'/assets/img/bg-log.png'; ?>'); background-size:600px;">
+                        <div class="col-lg-6" style="background-image: url('img/bg-log.png'); background-size:600px;">
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    <?php session_destroy(); ?>
     <!-- Footer-->
     <footer class="footer py-2 text-center" style="color: black; font-weight:bold; background-color: rgba(100, 162, 162, 0.9);">
         <span>Copyright © RD2 <?= date('Y'); ?></span>
@@ -94,14 +99,12 @@
     <!-- End of footer -->
 
     <!-- Bootstrap core JavaScript-->
-    <script src="<?= base_url ?>/assets/vendor/jquery/jquery.min.js"></script>
-    <script src="<?= base_url ?>/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-    <!-- Core plugin JavaScript-->
-    <script src="<?= base_url ?>/assets/vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
 
     <!-- Custom scripts for all pages-->
-    <script src="<?= base_url ?>/assets/js/sb-admin-2.min.js"></script>
+    <script src="js/sb-admin-2.min.js"></script>
 
 </body>
 
