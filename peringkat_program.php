16f2a9138aebcafeae97056d14a8a01b2d720939
Mauriza Humaira <mauri19@mhs.unsyiah.ac.id>
2021-05-26 14:13:45 +0700

    Menambahkan sidebar

---
+++ peringkat_program.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Peringkat Program</title>
+
+    <!-- Custom fonts for this template-->
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    <!-- Custom styles for this page -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+</head>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+     <!-- Sidebar -->
+<ul class="navbar-nav bg-gradient-white sidebar sidebar-dark accordion" id="accordionSidebar" style="border: 1px solid #137F7F;">
+
+<!-- Sidebar - Brand -->
+<a class="sidebar-brand d-flex align-items-center justify-content-center" href="#">
+    <img src="img/logo.png" width="90%;">
+</a>
+
+<div class="mx-auto my-2" style="background-color: #137F7F; height:1px; width: 150px;"></div>
+
+<!-- Nav Item - Dashboard -->
+<li class="nav-item">
+    <a class="nav-link font-weight-bold" style="color: black;" href="#">
+        <i class="fas fa-home" style="color: #137F7F;"></i>
+        <span>Menu Utama</span></a>
+</li>
+
+<div class="mx-auto my-2" style="background-color: #137F7F; height:1px; width: 150px;"></div>
+
+<!-- Nav Item - Pengajuan Program Menu -->
+<li class="nav-item">
+    <a class="nav-link collapsed font-weight-bold" style="color: black; " href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+        <i class="far fa-paper-plane" style="color: #137F7F;"></i>
+        <span>Pengajuan Program</span>
+    </a>
+    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+        <div class="bg-white py-2 collapse-inner rounded">
+            <a class="collapse-item font-weight-bold" style="color: black;" href="KirimUsulan.php">Kirim Pengajuan</a>
+            <a class="collapse-item font-weight-bold" style="color: black;" href="#">Riwayat Pengajuan</a>
+        </div>
+    </div>
+</li>
+
+<div class="mx-auto my-2" style="background-color: #137F7F; height:1px; width: 150px;"></div>
+
+<!-- Nav Item - Peringkat Program Menu -->
+<li class="nav-item">
+    <a class="nav-link collapsed font-weight-bold" style="color: black;" href="#">
+        <i class="far fa-star" style="color: #137F7F;"></i>
+        <span>Peringkat Program</span>
+    </a>
+</li>
+
+<div class="mx-auto my-2" style="background-color: #137F7F; height:1px; width: 150px;"></div>
+
+<!-- Nav Item - Logout Menu -->
+<li class="nav-item">
+    <a class="nav-link collapsed font-weight-bold" style="color: black;" href="#">
+        <i class="fas fa-sign-out-alt" style="color: #137F7F;"></i>
+        <span>Logout</span>
+    </a>
+</li>
+
+<div class="mx-auto my-2" style="background-color: #137F7F; height:1px; width: 150px;"></div>
+
+<!-- Sidebar Toggler (Sidebar) -->
+<div class="text-center d-none d-md-inline">
+    <button class="rounded-circle border-0" style="background-color: #137F7F;" id="sidebarToggle"></button>
+</div>
+
+</ul>
+<!-- End of Sidebar -->
