d6a67d583ef0adf72b8a1d70ef2da18c711fa47c
ulfiaakhairani <ulfia199@mhs.unsyiah.ac.id>
2021-06-01 19:33:59 +0700

    Menampilkan program yang diusulkan dan terlaksana

--- dashboard.php
+++ dashboard.php
@@ -212,6 +212,38 @@ $jumlah_usulan = mysqli_num_rows($countUsulan);
                                 </div>
                             </div>
                         </div>
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-success shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                                Usulan Program</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $jumlah_usulan ?></div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-hourglass-half" style="font-size: xx-large;"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-info shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                                Program Terlaksana</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $jumlah_program ?></div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-clipboard-check" style="font-size: xx-large;"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
 
                     <!-- Footer-->
