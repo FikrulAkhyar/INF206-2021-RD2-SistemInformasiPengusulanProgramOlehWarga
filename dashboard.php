c5e698cc303fcdc1567d8281159b4f34455953e7
ulfiaakhairani <ulfia199@mhs.unsyiah.ac.id>
2021-06-01 10:46:09 +0700

    Menampilkan jumlah warga

--- dashboard.php
+++ dashboard.php
@@ -193,6 +193,27 @@ $jumlah_usulan = mysqli_num_rows($countUsulan);
                 <!-- Content -->
                 <div class="container-fluid">
 
+                    <!-- Page Heading -->
+                    <h1 class="h3 mb-4 font-weight-bold" style="color: #137F7F">Menu Utama</h1>
+                    <div class="row justify-content-center">
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-primary shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                                Warga</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $jumlah_user ?></div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-users" style="font-size: xx-large;"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                     <!-- Footer-->
                     <footer class="footer py-2 text-center" style="color: black; font-weight:bold; background-color: rgba(100, 162, 162, 0.9);">
                         <span>Copyright © RD2 <?= date('Y'); ?></span>
@@ -201,3 +222,47 @@ $jumlah_usulan = mysqli_num_rows($countUsulan);
 
                 </div>
                 <!-- End of Content Wrapper -->
+                <!-- Scroll to Top Button-->
+                <a class="scroll-to-top rounded" href="#page-top">
+                    <i class="fas fa-angle-up"></i>
+                </a>
+
+                <!-- Logout Modal-->
+                <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Yakin Ingin Keluar ?</h5>
+                                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">×</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">Tekan Logout Untuk Keluar</div>
+                            <div class="modal-footer">
+                                <button class="btn btn-warning" type="button" data-dismiss="modal">Cancel</button>
+                                <a class="btn" style="background-color: rgba(100, 162, 162, 0.9); color:white" href="logout">Logout</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+                <!-- Bootstrap core JavaScript-->
+                <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+                <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+                <script src="js/kirimscript.js"></script>
+
+                <!-- Custom scripts for all pages-->
+                <script src="js/sb-admin-2.min.js"></script>
+
+                <!-- Page level plugins -->
+                <script src="vendor/datatables/jquery.dataTables.min.js"></script>
+                <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+                <!-- Page level custom scripts -->
+                <script src="js/datatables.js"></script>
+                <script src="js/kirimscript.js"></script>
+</body>
+
+</html>
