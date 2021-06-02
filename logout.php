8bf4aa2ed47a3ee4c5155e4b5f21812725f408d5
muhammad farhan anfasa <farhan_@mhs.unsyiah.ac.id>
2021-06-02 19:02:45 +0700

    reupload file

---
+++ logout.php
@@ -0,0 +1,7 @@
+<?php
+session_start();
+session_destroy();
+session_start();
+
+$_SESSION['message'] = '<div class="alert alert-success" role="alert">Berhasil Logout</div>';
+header("Location: login");
---
+++ register.php
@@ -0,0 +1,32 @@
+<?php
+require_once "config.php";
+
+$nik = htmlspecialchars($_POST["nik"]);
+$cekquery = mysqli_query($conn, "SELECT * FROM user where nik = '$nik'");
+$cekuser = mysqli_fetch_assoc($cekquery);
+
+if ($_POST["nik"] != $cekuser["nik"]) {
+    if ($_POST["password1"] == $_POST["password2"]) {
+        $nama = htmlspecialchars($_POST["nama"]);
+        $email = htmlspecialchars($_POST["email"]);
+        $image = "default.jpg";
+        $password = password_hash($_POST["password1"], PASSWORD_DEFAULT);
+        $no_hp = $_POST["no_hp"];
+        $id_status = 2;
+        $date_created = $date . " " . $time;
+
+        $insert = "INSERT INTO user VALUES ('', '$nik', '$nama', '$email', '$image', '$password', '$no_hp', '$id_status', '$date_created')";
+        if (mysqli_query($conn, $insert)) {
+            $flashdata = '<div class="alert alert-success" role="alert">Pendaftaran Akun berhasil, Silahkan Login</div>';
+            $_SESSION['flashdata'] = $flashdata;
+        } else {
+            echo "<script>alert('Gagal')</script>";
+        }
+    } else {
+        $_SESSION['passErr'] = "Password Tidak Sama";
+    }
+} else {
+    $_SESSION['nikErr'] = "NIK telah terdaftar";
+}
+
+header("Location:signup");
