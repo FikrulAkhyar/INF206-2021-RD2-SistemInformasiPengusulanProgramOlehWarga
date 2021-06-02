11b6e345af583474155079fe37f7b7bf59e6f971
Mauriza Humaira <mauri19@mhs.unsyiah.ac.id>
2021-06-02 17:15:07 +0700

    Menambahkan logika voting

--- server.php
+++ server.php
@@ -5,3 +5,33 @@ require_once "config.php";
 // lets assume a user is logged in with id $user_id
 $user_id = $_SESSION["id"];
 
+// if user clicks like or dislike button
+if (isset($_POST['action'])) {
+    $post_id = $_POST['post_id'];
+    $action = $_POST['action'];
+    switch ($action) {
+        case 'like':
+            $sql = "INSERT INTO rating_info (user_id, post_id, rating_action) 
+         	   VALUES ($user_id, $post_id, 'like') 
+         	   ON DUPLICATE KEY UPDATE rating_action='like'";
+            break;
+        case 'dislike':
+            $sql = "INSERT INTO rating_info (user_id, post_id, rating_action) 
+               VALUES ($user_id, $post_id, 'dislike') 
+         	   ON DUPLICATE KEY UPDATE rating_action='dislike'";
+            break;
+        case 'unlike':
+            $sql = "DELETE FROM rating_info WHERE user_id=$user_id AND post_id=$post_id";
+            break;
+        case 'undislike':
+            $sql = "DELETE FROM rating_info WHERE user_id=$user_id AND post_id=$post_id";
+            break;
+        default:
+            break;
+    }
+
+    // execute query to effect changes in the database ...
+    mysqli_query($conn, $sql);
+    echo getRating($post_id);
+    exit(0);
+}
