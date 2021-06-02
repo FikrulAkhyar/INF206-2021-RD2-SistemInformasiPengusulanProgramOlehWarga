76755ac9ef1fddf908be5a632764ce2b4e32201f
Mauriza Humaira <mauri19@mhs.unsyiah.ac.id>
2021-06-02 17:24:58 +0700

    Menambahkan handling function

--- js/vote.js
+++ js/vote.js
@@ -36,4 +36,40 @@ $(document).ready(function(){
     
     });
 
+    // if the user clicks on the dislike button ...
+    $('.dislike-btn').on('click', function(){
+      var post_id = $(this).data('id');
+      $clicked_btn = $(this);
+      if ($clicked_btn.hasClass('fa-thumbs-o-down')) {
+          action = 'dislike';
+      } else if($clicked_btn.hasClass('fa-thumbs-down')){
+          action = 'undislike';
+      }
+      $.ajax({
+          url: 'index.php',
+          type: 'post',
+          data: {
+              'action': action,
+              'post_id': post_id
+          },
+          success: function(data){
+              res = JSON.parse(data);
+              if (action == "dislike") {
+                  $clicked_btn.removeClass('fa-thumbs-o-down');
+                  $clicked_btn.addClass('fa-thumbs-down');
+              } else if(action == "undislike") {
+                  $clicked_btn.removeClass('fa-thumbs-down');
+                  $clicked_btn.addClass('fa-thumbs-o-down');
+              }
+              // display the number of likes and dislikes
+              $clicked_btn.siblings('span.likes').text(res.likes);
+              $clicked_btn.siblings('span.dislikes').text(res.dislikes);
+              
+              // change button styling of the other button if user is reacting the second time to post
+              $clicked_btn.siblings('i.fas fa-heart').removeClass('fas fa-heart').addClass('far fa-heart');
+          }
+      });	
+    
+    });
+
 });
