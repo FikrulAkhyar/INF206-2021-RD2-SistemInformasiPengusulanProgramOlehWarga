d5637d16e39902ad36171124eb7ae6232f4288d6
Mauriza Humaira <mauri19@mhs.unsyiah.ac.id>
2021-06-02 17:13:01 +0700

    Tampilan read program untuk voting

--- peringkat_program.php
+++ peringkat_program.php
@@ -57,3 +57,74 @@ $query = mysqli_query($conn, 'SELECT a.*, b.nama FROM usulan as a JOIN user as b
                             </form>
                         </div>
                     </div>
+                </div>
+            <?php } ?>
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                        <tr align="center">
+                            <th>No</th>
+                            <th>Nama Pengusul</th>
+                            <th>Judul</th>
+                            <th>Vote</th>
+                            <th style="width: 10px;"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $no = 1;
+                        while ($data = mysqli_fetch_assoc($query)) { ?>
+                            <tr>
+                                <td><?= $no; ?></td>
+                                <td><?= $data['nama']; ?></td>
+                                <td><?= $data['judul']; ?></td>
+                                <td align="center">
+                                    <span class="likes"><?php echo getLikes($data['id']); ?></span>
+                                    <i <?php if (userLiked($data['id'])) : ?> class="fas fa-heart like-btn" <?php else : ?> class="far fa-heart like-btn" <?php endif ?> data-id="<?php echo $data['id'] ?>" style="color: red;"></i>
+                                </td>
+                                <td><button type="button" class="btn" data-toggle="modal" data-target="#detail<?= $no; ?>"><i class="fas fa-arrow-right" style="color: #137F7F"></i></button></td>
+                            </tr>
+                            <!-- Modal -->
+                            <div class="modal fade" id="detail<?= $no++; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-centered" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLongTitle">Detail Usulan</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body mx-4">
+                                            <form>
+                                                <div class="form-group">
+                                                    <label for="nama">Nama</label>
+                                                    <input type="text" class="form-control" id="nama" value="<?= $data['nama']; ?>" readonly>
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="judul">Judul Usulan</label>
+                                                    <input type="text" class="form-control" id="judul" value="<?= $data['judul']; ?>" readonly>
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="deskripsi">Dekripsi Usulan</label>
+                                                    <textarea class="form-control" id="deskripsi" rows="3" readonly><?= $data['deskripsi']; ?></textarea>
+                                                </div>
+                                            </form>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        <?php } ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
