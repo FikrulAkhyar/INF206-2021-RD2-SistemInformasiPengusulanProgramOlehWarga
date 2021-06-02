77c49a14d02eadba0af4af866728886b265c7db5
muhammad farhan anfasa <farhan_@mhs.unsyiah.ac.id>
2021-06-02 17:59:40 +0700

    hapus file

--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_destroy();
-session_start();
-
-$_SESSION['message'] = '<div class="alert alert-success" role="alert">Berhasil Logout</div>';
-header("Location: login");
